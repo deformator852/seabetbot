a93d3ed291d6ded7fe9d7385ce1246fcbbc8074d
deformator852 <83163608+deformator852@users.noreply.github.com>
2023-11-06 03:12:10 -0500

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,10 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 
-TOKEN = "6197552285:AAGCO_BOGXIWuwLpREaio4wMcSNtrsDXANc"
+TOKEN = ""
 bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 
 dp = Dispatcher()
 root_admins = [
-    46629637,
+    
 ]
